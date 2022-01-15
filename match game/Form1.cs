1c2eea39efe1c0e93fe9ff080b542ae1ef05a45d
zinebabila <zineb.abila.29@gmail.com>
2022-01-15 15:16:47 +0100

    validation

--- match game/Form1.cs
+++ match game/Form1.cs
@@ -12,14 +12,15 @@ namespace match_game
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
+        // firstClicked points to the first Label control 
+        // that the player clicks, but it will be null 
+        // if the player hasn't clicked a label yet
+        Label firstClicked = null;
 
-            AssignIconsToSquares();
-            
-        }
-
+        // secondClicked points to the second Label control 
+        // that the player clicks
+        Label secondClicked = null;
+        // Use this Random object to choose random icons for the squares
         Random random = new Random();
 
         // Each of these letters is an interesting icon
@@ -30,11 +31,10 @@ namespace match_game
         "!", "!", "N", "N", ",", ",", "k", "k",
         "b", "b", "v", "v", "w", "w", "z", "z"
     };
-        Label firstClicked = null;
 
-        // secondClicked points to the second Label control 
-        // that the player clicks
-        Label secondClicked = null;
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
         private void AssignIconsToSquares()
         {
             // The TableLayoutPanel has 16 labels,
@@ -48,15 +48,35 @@ namespace match_game
                 {
                     int randomNumber = random.Next(icons.Count);
                     iconLabel.Text = icons[randomNumber];
-                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    iconLabel.ForeColor = iconLabel.BackColor;
                     icons.RemoveAt(randomNumber);
                 }
             }
         }
+        public Form1()
+        {
+            InitializeComponent();
 
-        private void label_Click(object sender, EventArgs e)
+            AssignIconsToSquares();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+
+        /// <summary>
+        /// Every label's Click event is handled by this event handler
+        /// </summary>
+        /// <param name="sender">The label that was clicked</param>
+        /// <param name="e"></param>
+        private void label_Click(object sender, EventArgs e)
+        {
+            // The timer is only on after two non-matching 
+            // icons have been shown to the player, 
+            // so ignore any clicks if the timer is running
             if (timer1.Enabled == true)
                 return;
 
@@ -88,8 +108,12 @@ namespace match_game
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
-
+                // Check to see if the player won
                 CheckForWinner();
+
+                // If the player clicked two matching icons, keep them 
+                // black and reset firstClicked and secondClicked 
+                // so the player can click another icon
                 if (firstClicked.Text == secondClicked.Text)
                 {
                     firstClicked = null;
@@ -97,11 +121,22 @@ namespace match_game
                     return;
                 }
 
+                // If the player gets this far, the player 
+                // clicked two different icons, so start the 
+                // timer (which will wait three quarters of 
+                // a second, and then hide the icons)
                 timer1.Start();
-
             }
-            }
+        }
 
+        /// <summary>
+        /// This timer is started when the player clicks 
+        /// two icons that don't match,
+        /// so it counts three quarters of a second 
+        /// and then turns itself off and hides both icons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void timer1_Tick(object sender, EventArgs e)
         {
             // Stop the timer
@@ -111,13 +146,18 @@ namespace match_game
             firstClicked.ForeColor = firstClicked.BackColor;
             secondClicked.ForeColor = secondClicked.BackColor;
 
-            
             // Reset firstClicked and secondClicked 
             // so the next time a label is
             // clicked, the program knows it's the first click
             firstClicked = null;
             secondClicked = null;
         }
+
+        /// <summary>
+        /// Check every icon to see if it is matched, by 
+        /// comparing its foreground color to its background color. 
+        /// If all of the icons are matched, the player wins
+        /// </summary>
         private void CheckForWinner()
         {
             // Go through all of the labels in the TableLayoutPanel, 
